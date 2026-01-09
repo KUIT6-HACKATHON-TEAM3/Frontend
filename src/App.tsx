d5ca6877d0aa498bf5ab08f080dac09ceb8938f1
namsunghyun3057 <namsunghyen453@gmail.com>
2026-01-10 01:46:16 +0900

    delete: privateRoute

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import type { JSX } from 'react';
 import { pointsByRoad  } from "./data/all_roads_walking_paths"
 import MapPage from "./pages/MapPage";
-
-function PrivateRoute({ children }: { children: JSX.Element }) {
-  // 로컬 스토리지에 토큰이 있는지 확인
-  const token = localStorage.getItem("accessToken");
-  
-  // 토큰 있으면 통과(children), 없으면 로그인 페이지로 이동
-  return token ? children : <Navigate to="/login" replace />;
-}
 
 function App() {
   return (
