b191c83459138070231966d1010e4049180b9016
namsunghyun3057 <namsunghyen453@gmail.com>
2026-01-09 19:06:23 +0900

    fix: icons

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios"; // npm install axios
-import logoImg from "../assets/logo.png"
+import logoImg from "../assets/icons/logo.png"
 
 export default function Login() {
     const navigate = useNavigate();
